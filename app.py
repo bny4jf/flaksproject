bcb724dc6106d60e36deec721093fbead14cdd07
bny4jf <toth.peter@utexas.edu>
2020-06-26 11:13:22 -0700

    What is happening

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request, redirect
+import ploting
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+
+  if request.method=='POST':
+    ticker = request.form['ticker']
+    series = request.form.getlist('series')
+    try:
+        fig = ploting.plot_series(600, 300, ticker, ploting.getdata(ticker), series)
+        script, div= components(fig)
+    except:
+        print("Try another ticker symbol.")
+
   return render_template('index.html')
 
 @app.route('/about')
